b9a3060cf9946bd6bf371ef7cdc20aa707e9b4a9
not a duck <kaitlynewing@gmail.com>
2018-10-27 21:48:38 -0400

    ..

--- ComponentFramework/Scene3.cpp
+++ ComponentFramework/Scene3.cpp
@@ -25,7 +25,15 @@ bool Scene3::OnCreate()
 
 void Scene3::OnResize(int w_, int h_)
 {
+	windowPtr->SetWindowSize(w_, h_);
+	glViewport(0, 0, windowPtr->GetWidth(), windowPtr->GetHeight());
+	float aspect = float(windowPtr->GetWidth()) / float(windowPtr->GetHeight());
 
+	projectionMatrix = MMath::perspective(45.0f, aspect, 1.0f, 100.0f);
+
+	viewMatrix = MMath::lookAt(Vec3(0.0f, 0.0f, 10.0f),
+		Vec3(0.0f, 0.0f, 0.0f),
+		Vec3(0.0f, 1.0f, 0.0f));
 }
 
 void Scene3::OnDestroy()
@@ -39,6 +47,7 @@ void Scene3::Update(const float deltaTime)
 
 void Scene3::Render() const
 {
+
 }
 
 void Scene3::HandleEvents(const SDL_Event& SDLEvent)
