066d849b67fce854f82a418500ced1f754688930
not a duck <kaitlynewing@gmail.com>
2018-10-27 23:15:25 -0400

    idek

--- ComponentFramework/Model0.h
+++ ComponentFramework/Model0.h
@@ -3,43 +3,45 @@
 #include <GL/glew.h>
 #include "Model.h"
 
-namespace GAME {
+namespace GAME
+{
 
-/// Forward declarations;
-class Shader; 
-class QuadSphere;
+	/// Forward declarations;
+	class Shader;
+	class QuadSphere;
 
-class Model0 :public Model {
-public:
-	Model0();
+	class Model0 :public Model
+	{
+	public:
+		Model0();
 
 
-	Model0(const Model0&) = delete;
-	Model0(Model0&&) = delete;
-	Model0& operator = (const Model0&) = delete;
-	Model0& operator = (Model0&&) = delete;
+		Model0(const Model0&) = delete;
+		Model0(Model0&&) = delete;
+		Model0& operator = (const Model0&) = delete;
+		Model0& operator = (Model0&&) = delete;
 
-	virtual bool OnCreate() ;
-	virtual void OnDestroy();
-	virtual void Update(const float deltaTime);
-	virtual void Render(const Matrix4& projectionMatrix, const Matrix4& modelViewMatrix, const Matrix3& normalMatrix) const;
-	virtual ~Model0();
+		virtual bool OnCreate();
+		virtual void OnDestroy();
+		virtual void Update(const float deltaTime);
+		virtual void Render(const Matrix4& projectionMatrix, const Matrix4& modelViewMatrix, const Matrix3& normalMatrix) const;
+		virtual ~Model0();
 
-	void SetLightPos(const Vec3& );
-private:
-             
-	Shader *shader;
-	GLuint vbo;
-	GLuint vao;
-	GLint verticiesID;
-	GLint normalsID;
-	GLint uvCoordsID; 
+		void SetLightPos(const Vec3&);
+	private:
 
-	GLint projectionMatrixID;
-	GLint modelViewMatrixID;
-	GLint normalMatrixID;
-	GLint lightPosID;
-	Vec3 lightPos;
-};
+		Shader *shader;
+		GLuint vbo;
+		GLuint vao;
+		GLint verticiesID;
+		GLint normalsID;
+		GLint uvCoordsID;
+
+		GLint projectionMatrixID;
+		GLint modelViewMatrixID;
+		GLint normalMatrixID;
+		GLint lightPosID;
+		Vec3 lightPos;
+	};
 }
 #endif
