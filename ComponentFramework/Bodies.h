55f2d47a9eda61ae5ec97fc8a097a1710fc1a000
Richard Dela Merced <Delamercedrichard@gmail.com>
2018-11-05 15:41:53 -0500

    Fixing weird Errors

--- ComponentFramework/Bodies.h
+++ ComponentFramework/Bodies.h
@@ -8,14 +8,14 @@ class Bodies
 	friend class BodyModifier;
 protected:
 	Vec3 position;
-<<<<<<< HEAD
+
 	Vec3 rotationDirection;
 	float rotationAngle;
 	GAME::Model0 model; 
-=======
+
 	Vec3 rotation;
 	Model0 *model;
->>>>>>> fc52593aa6a4719842114f4017795b2c59af2d67
+
 public:
 	Bodies(Vec3 pos, Vec3 rot, Model0* m);
 	Bodies();
