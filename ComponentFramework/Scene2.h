ceeefaf896e47e512b36e3507f9f53f7d3c5f2fe
not a duck <kaitlynewing@gmail.com>
2018-11-06 18:51:14 -0500

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/decadentduck/GAME203Assignment2"""
    This reverts commit 09eafa1a53c39dd46a1b645895fb9008ae9f1c5e.

--- ComponentFramework/Scene2.h
+++ ComponentFramework/Scene2.h
@@ -1,42 +1,37 @@
 #ifndef Scene2_H
 #define Scene2_H
-
+#include <GL/glew.h>
+#include <SDL.h>
 #include "Scene.h"
 #include "Window.h"
-
-
-
-namespace GAME {
+#include <iostream>
+#include "MMath.h"
+#include "QuadSphere.h"
+#include "Trackball.h"
+///deorating objects assignment
+namespace GAME
+{
 	/// Forward casting
 	class Trackball;
-	class Model1;
+	class Model;
 
-	class Scene2 : public Scene {
+	class Scene2 : public Scene
+	{
 	protected:
 
 	public:
 		explicit Scene2(Window& windowRef);
 		virtual ~Scene2();
-
-
-		/// Delete these possible default constructors and operators  
-		Scene2(const Scene2&) = delete;
-		Scene2(Scene2 &&) = delete;
-		Scene2& operator=(const Scene2 &) = delete;
-		Scene2& operator=(Scene2 &&) = delete;
-
 		virtual bool OnCreate();
 		virtual void OnDestroy();
 		virtual void Update(const float deltaTime);
 		virtual void Render() const;
 		virtual void HandleEvents(const SDL_Event &SDLEvent);
 		virtual void OnResize(const int, const int);
-
 	private:
 		Vec3 lightPos;
-		Model1 *model1;
+		Model *model;
 		Trackball *trackball;
-
 	};
 }
 #endif
